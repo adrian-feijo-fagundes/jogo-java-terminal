48148a3ebc1ec53948eca54525aa508dcf51d8cf
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-11-22 11:36:24 -0300

    Implementando setGold e getGold da classe Player.java

--- src/entities/playable/Player.java
+++ src/entities/playable/Player.java
@@ -1,6 +1,9 @@
-package entities;
+package entities.playable;
 import java.util.ArrayList;
 import java.util.List;
+
+import entities.Character;
+import entities.Creature;
 
 public class Player extends Creature implements Character{
     private List<String> itensBag;
@@ -11,8 +14,12 @@ public class Player extends Creature implements Character{
         itensBag = new ArrayList<>();
     }
 
-    public void getGold() {}
-    public void setGold() {}
+    public double getGold() {
+        return this.gold;
+    }
+    public void setGold(double gold) {
+        this.gold = gold;
+    }
     public void openInventory() {}
     @Override
     public void attack() {}
