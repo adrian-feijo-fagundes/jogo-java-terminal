02c0723c1ab54c0c708edd00aa19ea6b7afbeabc
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2024-11-22 21:44:42 -0300

    Terminando implementação da criação do personagem

--- src/App.java
+++ src/App.java
@@ -19,7 +19,7 @@ public class App {
         //
         // 
         //
-
+        sc.close();
     }
     public static Player createPlayer(Scanner sc) {
         System.out.println("\nQual o nome do seu Personagem?");
@@ -32,34 +32,39 @@ public class App {
         System.out.println("4. Mercador");
         System.out.println("Digite sua escolha"); 
         int choice = sc.nextInt();
-        
-        System.out.println("\n Agora vamos rolar os dados para calcular os atributos do seu personagem");
-        System.out.println("\n Rolando dados para o HP (vida)");
+        while (choice < 1 || choice > 4) {
+            System.out.println("Escolha uma opção válida");
+            choice = sc.nextInt();
+        }
+        System.out.println("\nAgora vamos rolar os dados para calcular os atributos do seu personagem");
+        System.out.println("\nRolando dados para o HP (vida)");
         int life = Dice.roll(sc, 3, 6);
-        System.out.println("\n Rolando dados para o MP (mana)");
+        System.out.println("\nRolando dados para o MP (mana)");
         int mana = Dice.roll(sc, 3, 6);
-        System.out.println("\n Rolando dados para o ATK (ataque)");
+        System.out.println("\nRolando dados para o ATK (ataque)");
         int attack = Dice.roll(sc, 2, 6);
-        System.out.println("\n Rolando dados para o DEF (defesa)");
+        System.out.println("\nRolando dados para o DEF (defesa)");
         int defense = Dice.roll(sc, 2, 6);
-        System.out.println("\n Rolando dados para o SPD (velocidade)");
+        System.out.println("\nRolando dados para o SPD (velocidade)");
         int speed = Dice.roll(sc, 2, 6);
-        System.out.println("\n Rolando dados para o LUCK (sorte)");
+        System.out.println("\nRolando dados para o LUCK (sorte)");
         int luck = Dice.roll(sc, 2, 6);
-        do {
-            switch (choice) {
-                case 1:
-                    return new Hunter(name, life, attack, mana, defense, speed, luck);
-                case 2:
-                    return new Priest(name, life, attack, mana, defense, speed, luck);
-                case 3:
-                    return new Warrior(name, life, attack, mana, defense, speed, luck);
-                case 4:
-                    return new Merchant(name, life, attack, mana, defense, speed, luck);
-                default:
-                    System.out.println("Escolha uma opção válida");
-                    break;
-            }    
-        } while (true);     
+        Player player = null;
+        switch (choice) {
+            case 1:
+                player = new Hunter(name, life, attack, mana, defense, speed, luck);
+                break;
+            case 2:
+                player = new Priest(name, life, attack, mana, defense, speed, luck);
+                break;
+            case 3:
+                player = new Warrior(name, life, attack, mana, defense, speed, luck);
+                break;
+            case 4:
+                player = new Merchant(name, life, attack, mana, defense, speed, luck);
+                break;
+        
+        }
+        return player;         
     }
 }
