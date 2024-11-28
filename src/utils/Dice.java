11f9abf5621b67924eb1375f77f6b724aa344731
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-11-28 16:00:10 -0300

    Atualizando Interface Entity para uma classe e implementando Dice.simpleRoll()

---
+++ src/utils/Dice.java
@@ -0,0 +1,21 @@
+package utils;
+import java.util.Scanner;
+
+public abstract class Dice {
+    public static int roll(Scanner sc, int timesToRoll, int sides) {
+        int total = 0;
+        for (int i = 0; i < timesToRoll; i++) {
+            System.out.println("Pressione Enter para rolar o dado " + (i+1) + " de " + timesToRoll + " (d" + sides + ")");
+            sc.nextLine();
+            int result = (int) (Math.random() * sides) + 1;
+            System.out.println("Voce rolou:" + result);
+            total += result;
+        }
+
+        return total; 
+    }
+
+    public static int simpleRoll(int sides) {
+        return (int) (Math.random() * sides) + 1;
+    }
+}
---
+++ src/utils/Entity.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Entity {
+    private String name; 
+
+    public Entity(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
