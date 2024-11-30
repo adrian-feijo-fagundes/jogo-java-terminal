b54bb1db243dc594d7e4a85d16b34a16164319ed
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2024-11-29 21:42:09 -0300

    removendo clearScreen do classe App.java

--- rpg_terminal/app/src/main/java/rpg_terminal/App.java
+++ rpg_terminal/app/src/main/java/rpg_terminal/App.java
@@ -71,9 +71,4 @@ public class App {
         }
         return player;
     }
-
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
 }
