00dd703a26472d055c29e1d43ead15b02239d276
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-12-04 11:25:51 -0300

    Update Scene & Create TestingScenes.java

--- rpg_terminal/app/src/main/java/rpg_terminal/scenarios/Scene.java
+++ rpg_terminal/app/src/main/java/rpg_terminal/scenarios/Scene.java
@@ -4,19 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import rpg_terminal.Screen;
 import rpg_terminal.entities.creatures.Creature;
 
 public class Scene {
-    private final String description;
+    private final List<String> messages;
     private final List<Option> options;
 
-    public Scene(String description) {
-        this.description = description;
+    public Scene(List<String> messages) {
+        this.messages = new ArrayList<>(messages);
         this.options = new ArrayList<>();
     }
 
-    public String getDescription() {
-        return description;
+    public List<String> getMessages() {
+        return messages;
     }
 
     public List<Option> getOptions() {
@@ -28,7 +29,21 @@ public class Scene {
     }
 
     public void startEvent(Scanner sc, Creature player) {
-        System.out.println(description);
+        // Ainda vou utilizar o "player" para iterações futuras
+        // Muito provavelmente vou ir dividindo o código para ficar mais organizado está
+        // ainda é uma versão inicial
+        // Mostrar mensagens
+        for (String message : this.messages) {
+            Screen.clear();
+            System.out.println(message);
+            if (message != this.messages.get(this.messages.size() - 1)) {
+                System.out.println("\nPRESSIONE ENTER PARA CONTINUAR");
+                sc.nextLine();
+            }
+        }
+        System.out.println("\nO que fazer?");
+        // Vou inserir as opções aqui
+        sc.nextInt();
     }
 
 }
---
+++ temp/SistemaDeCenas.md
@@ -0,0 +1,40 @@
+## Como eu vou fazer o sistema de cenas / Lopping de cenas
+
+- Quando começar uma cena é necessario:
+
+  - Mostrar mensagem:
+    Podem ser várias mensagens
+    Exemplo: "Voce sozinho em uma sala"
+    O que deseja fazer?
+
+  - Mostrar opções
+
+    - A quantidade de opções pode mudar dependendo da cena
+    - As opções levam para outra cena
+
+  - Existem mais de um tipo de cena
+  - Algumas cenas necessitam de um pré-requisito para serem acessadas
+
+## Lógica
+
+    - Exibir mensagens cena atual
+    - Exibir opções
+    - Se estiver tudo bem levar para a proxima cena
+
+## Especifico
+
+    - Cenas "Bloqueadas"
+    - Cenas/eventos de Itens
+    - Cenas/eventos de Combat
+
+## Tarefas
+
+    - Fazer cena simples funcionar
+    - Fazer combate acontecer
+    - Fazer eventos de itens
+        - Loja
+        - Ganhar itens aleatórios
+        - Sistema de drop dos monstros
+
+need item = true
+verifica o inventário do jogador
