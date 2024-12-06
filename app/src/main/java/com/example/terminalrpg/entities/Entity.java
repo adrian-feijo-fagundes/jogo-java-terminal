ac3156f0ce1646c2a7288205b322d6af6af222c3
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-12-06 14:18:18 -0300

    Base da classe Entity

--- app/src/main/java/com/example/terminalrpg/entities/Entity.java
+++ app/src/main/java/com/example/terminalrpg/entities/Entity.java
@@ -9,5 +9,23 @@ package com.example.terminalrpg.entities;
  * @author Adrian
  */
 public class Entity {
-    
+    private final String id;
+    private String name;
+
+    public Entity(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
