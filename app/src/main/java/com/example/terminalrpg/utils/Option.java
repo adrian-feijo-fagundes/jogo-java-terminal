d9390bbb3b1a3c9b8d464549ec94061566f47bd7
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-12-06 16:28:59 -0300

    Another changes

--- app/src/main/java/com/example/terminalrpg/utils/Option.java
+++ app/src/main/java/com/example/terminalrpg/utils/Option.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
  */
-package com.example.terminalrpg.entities.scene;
+package com.example.terminalrpg.utils;
+
+import com.example.terminalrpg.entities.scene.Scene;
 
 /**
  *
